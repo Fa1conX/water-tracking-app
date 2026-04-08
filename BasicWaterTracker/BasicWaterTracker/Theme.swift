3abc2fafbcf8bdc7cfe5bd66875ac4b776b62440
Fa1conX <fa1conX01@protonmail.com>
2026-04-08 09:49:00 -0700

    changed light mode color background to be nicer

--- BasicWaterTracker/BasicWaterTracker/Theme.swift
+++ BasicWaterTracker/BasicWaterTracker/Theme.swift
@@ -12,7 +12,7 @@ extension Color {
         Color(UIColor { traitCollection in
             traitCollection.userInterfaceStyle == .dark
                 ? UIColor(red: 0.1608, green: 0.1647, blue: 0.1686, alpha: 1.0)
-                : UIColor(red: 160.0 / 255.0, green: 160.0 / 255.0, blue: 165.0 / 255.0, alpha: 1.0)
+                : UIColor(red: 230.0 / 255.0, green: 236.0 / 255.0, blue: 248.0 / 255.0, alpha: 1.0)
         })
     }
 }
